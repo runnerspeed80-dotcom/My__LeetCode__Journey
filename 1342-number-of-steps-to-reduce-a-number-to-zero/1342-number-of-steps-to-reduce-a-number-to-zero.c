ede03861104b71a3390cedd83b7287777b3f669e
runnerspeed80-dotcom <runnerspeed80@gmail.com>
2026-09-08 20:25:33 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.88%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.c
@@ -0,0 +1,22 @@
+int numberOfSteps(int num) {
+
+    int sum = 0;
+    
+
+    while(num>0){
+        if(num % 2 == 0){
+            
+            num = num/2;
+            sum++;
+        }
+        else{
+            num = num -1;
+            
+            sum++;
+        }
+    }
+
+    return sum;
+    
+    
+}
