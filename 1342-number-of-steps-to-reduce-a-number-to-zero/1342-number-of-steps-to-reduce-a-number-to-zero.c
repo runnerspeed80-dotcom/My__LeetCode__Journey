c4c7ea1fe6c7628d1bb9097b484a2083b0d1bad4
runnerspeed80-dotcom <runnerspeed80@gmail.com>
2026-09-08 21:23:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (31.68%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.c
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.c
@@ -1,22 +1,14 @@
 int numberOfSteps(int num) {
-
     int sum = 0;
-    
-
     while(num>0){
-        if(num % 2 == 0){
-            
+        if(num % 2 == 0){            
             num = num/2;
             sum++;
         }
         else{
-            num = num -1;
-            
+            num = num -1;            
             sum++;
         }
     }
-
-    return sum;
-    
-    
+    return sum;    
 }
