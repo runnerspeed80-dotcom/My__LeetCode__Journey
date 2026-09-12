6fde39a6fbce0bdaea4747325c7bba84e2bc7754
runnerspeed80-dotcom <runnerspeed80@gmail.com>
2026-09-12 11:19:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (11.57%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.c
@@ -0,0 +1,29 @@
+int com(const void *a ,const void *b){
+    return (*(int*)a - *(int*)b);
+}
+
+
+int heightChecker(int* heights, int heightsSize) {
+
+    int expected[heightsSize];
+    for(int i = 0; i <heightsSize ; i++){
+        expected[i]=heights[i];
+    }  
+    int total = 0;
+
+    qsort(expected , heightsSize , sizeof(int) , com);
+
+    for(int i = 0; i <heightsSize ; i++){
+        if(heights[i] != expected[i]){
+            total +=1;
+        }   
+
+
+    }
+
+    return total;
+        
+
+    
+    
+}
