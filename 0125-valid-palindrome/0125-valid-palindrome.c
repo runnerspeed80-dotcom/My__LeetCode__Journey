a7ec1a9303118f27574713b8e17e2f5479bd1399
runnerspeed80-dotcom <runnerspeed80@gmail.com>
2026-09-19 22:32:29 +0530

    Time: 1 ms (31.22%), Space: 9.5 MB (6.45%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -1,33 +1,24 @@
 bool isPalindrome(char* s) {
     int len = strlen(s);
-    char final[len+1];
+    char f[len+1];
     int j=0;
-
-    for(int i = 0; i<len; i++){
+    for(int i = 0; i<len ; i++){
         if(isalnum(s[i])){
-
-            final[j] = s[i];
+            f[j] = s[i];
             j++;
-            
-
         }
     }
-    final[j]= '\0';
+    f[j] = '\0';
 
-    int flen = strlen(final);
+    int flen = strlen(f);
 
-    for(int i =0;i<flen ; i++){
-        final[i] = tolower(final[i]);
+    for(int i = 0; i<flen ; i++ ){
+        f[i] = tolower(f[i]);
     }
-
-    for(int i = 0; i < flen/2 ; i++){
-        if(final[i] != final[flen-1-i] ){
+    for(int i = 0 ; i < flen/2 ; i++){
+        if(f[i]!=f[flen-1-i] ){
             return false;
         }
-
-
     }
-
     return true;
-
 }
